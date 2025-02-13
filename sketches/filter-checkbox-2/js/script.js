345feeabf48ac12493996b886791133ee581a8fd
rupali <36826017+rupalimo@users.noreply.github.com>
2025-02-13 00:42:36 -0500

    update new styling on third option

--- sketches/filter-checkbox-2/js/script.js
+++ sketches/filter-checkbox-2/js/script.js
@@ -20,3 +20,23 @@ function openTab(evt, tabName) {
 }
 
 document.getElementById('defaultOpen').click();
+
+// for the news recent vs old toggle
+function showNews(evt, tabName) {
+	var newsTab = document.getElementsByClassName('para__news');
+	for (var i = 0; i < newsTab.length; i++) {
+		newsTab[i].style.display = 'none';
+	}
+
+	// Get all elements with class="tabitems" and remove the class "active"
+	newsItem = document.getElementsByClassName('news--tab');
+	for (i = 0; i < newsItem.length; i++) {
+		newsItem[i].className = newsItem[i].className.replace(' on', '');
+	}
+
+	// Show the current tab, and add an "active" class to the button that opened the tab
+	document.getElementById(tabName).style.display = 'block';
+	evt.currentTarget.className += ' on';
+}
+
+document.getElementById('defaultNow').click();
