a640e5c178fca4a75720f1c1481a29b3ce8a10ea
rupali <36826017+rupalimo@users.noreply.github.com>
2025-02-12 20:49:40 -0500

    add in news subtab

--- sketches/filter-checkbox-1/js/script.js
+++ sketches/filter-checkbox-1/js/script.js
@@ -20,3 +20,21 @@ function openTab(evt, tabName) {
 }
 
 document.getElementById('defaultOpen').click();
+
+// for the news recent vs old toggle
+function showNews(evt, tabName) {
+	var newsTab = document.getElementsByClassName('para__news');
+	for (var i = 0; i < newsTab.length; i++) {
+		newsTab[i].style.display = 'none';
+	}
+
+	// Get all elements with class="tabitems" and remove the class "active"
+	newsItem = document.getElementsByClassName('news--toggle');
+	for (i = 0; i < newsItem.length; i++) {
+		newsItem[i].className = newsItem[i].className.replace(' btn--on', '');
+	}
+
+	// Show the current tab, and add an "active" class to the button that opened the tab
+	document.getElementById(tabName).style.display = 'block';
+	evt.currentTarget.className += ' btn--on';
+}
